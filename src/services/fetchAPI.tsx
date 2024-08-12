b0636a90f29d911c2242bf9a4656d70766737ab5
Seu nome <yago_bohmcruz@hotmail.com>
2024-08-12 13:36:06 -0300

    Desployment test

--- src/services/fetchAPI.tsx
+++ src/services/fetchAPI.tsx
@@ -2,12 +2,15 @@ import { Employees } from "../types/Employees";
 
 const fetchEmployees = async (): Promise<Employees[]> => {
   try {
-    const response = await fetch("http://localhost:3000/employees");
+    const response = await fetch(
+      "https://yaguera021.github.io/project-data/db.json",
+    );
     if (!response.ok) {
       throw new Error("Failed to fetch employees");
     }
-    const data: Employees[] = await response.json();
-    return data;
+    const data = await response.json();
+
+    return data.employees;
   } catch (error) {
     console.error("Error fetching employees:", error);
     return [];
